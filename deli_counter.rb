c67e3f1406806a16d3cb882d7e962a210163939c
Aziz Nasirov <nasirovaziz@yahoo.com>
2020-02-18 03:08:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,3 +1,17 @@
 katz_deli = []
+
 def line(katz_deli)
-  curren
+  line_postitions = [];
+  if katz_deli.empty?
+    puts "The line is currently empty."
+  else
+    number = 1
+    katz.deli.each do |customer|
+      line_postitions << "#{number}. #{customer}"
+      number += 1
+    end
+    puts "The line is currently: #{line_positions.join(" ")}"
+  end
+end
+
+def
