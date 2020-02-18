438efd6510e6a35bdfa65059bb7d867727957959
Aziz Nasirov <nasirovaziz@yahoo.com>
2020-02-18 03:16:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     number = 1
-    katz.deli.each do |customer|
+    katz_deli.each do |customer|
       line_positions << "#{number}. #{customer}"
       number += 1
     end
@@ -15,7 +15,7 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
-  Katz_deli << name
+  katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end
 
@@ -23,7 +23,7 @@ def now_serving(katz_deli)
   if katz_deli.empty?
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{katz_deli.first}"
+    puts "Currently serving #{katz_deli.first}."
     katz_deli.shift
   end
 end
